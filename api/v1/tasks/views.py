76393c7532e040cf9aa5c84fea0138781e4e972a
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-09-11 15:29:33 +0530

    Introduction and Project Setup(Tasks List API)

--- api/v1/tasks/views.py
+++ api/v1/tasks/views.py
@@ -1,7 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from tasks.models import Task
+from api.v1.tasks.serializers import TaskSerializer
+
 
 @api_view(["GET"])
 def tasks(request):
-    return Response("Hello")
+    instance = Task.objects.filter(is_deleted=False)
+    serializer = TaskSerializer(instance, many=True)
+
+    return Response(serializer.data)
